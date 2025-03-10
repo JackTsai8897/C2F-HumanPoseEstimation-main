c6d1e5e7cd95ad6484a70604790e979ecbb57f60
Jack Tsai <goldenyami8897@gmail.com>
2025-03-11 01:57:36 +0800

    Generate input image of model from coco dataset fix: update dataset configuration and cleanup
    - Add tools/output_images/* to .gitignore
    - Fix DATASET.SCALE_FACTOR type (0 to 0.0)
    - Uncomment COCO dataset import
    - Simplify COCO evaluation by removing unused NMS imports
    - Remove output image files from git tracking

--- lib/dataset/coco.py
+++ lib/dataset/coco.py
@@ -19,8 +19,8 @@ import json_tricks as json
 import numpy as np
 
 from dataset.JointsDataset import JointsDataset
-from nms.nms import oks_nms
-from nms.nms import soft_oks_nms
+# from nms.nms import oks_nms
+# from nms.nms import soft_oks_nms
 
 
 logger = logging.getLogger(__name__)
@@ -286,84 +286,8 @@ class COCODataset(JointsDataset):
             self.image_thre, num_boxes))
         return kpt_db
 
-    def evaluate(self, cfg, preds, output_dir, all_boxes, img_path,
-                 *args, **kwargs):
-        rank = cfg.RANK
-
-        res_folder = os.path.join(output_dir, 'results')
-        if not os.path.exists(res_folder):
-            try:
-                os.makedirs(res_folder)
-            except Exception:
-                logger.error('Fail to make {}'.format(res_folder))
-
-        res_file = os.path.join(
-            res_folder, 'keypoints_{}_results_{}.json'.format(
-                self.image_set, rank)
-        )
-
-        # person x (keypoints)
-        _kpts = []
-        for idx, kpt in enumerate(preds):
-            _kpts.append({
-                'keypoints': kpt,
-                'center': all_boxes[idx][0:2],
-                'scale': all_boxes[idx][2:4],
-                'area': all_boxes[idx][4],
-                'score': all_boxes[idx][5],
-                'image': int(img_path[idx][-16:-4])
-            })
-        # image x person x (keypoints)
-        kpts = defaultdict(list)
-        for kpt in _kpts:
-            kpts[kpt['image']].append(kpt)
-
-        # rescoring and oks nms
-        num_joints = self.num_joints
-        in_vis_thre = self.in_vis_thre
-        oks_thre = self.oks_thre
-        oks_nmsed_kpts = []
-        for img in kpts.keys():
-            img_kpts = kpts[img]
-            for n_p in img_kpts:
-                box_score = n_p['score']
-                kpt_score = 0
-                valid_num = 0
-                for n_jt in range(0, num_joints):
-                    t_s = n_p['keypoints'][n_jt][2]
-                    if t_s > in_vis_thre:
-                        kpt_score = kpt_score + t_s
-                        valid_num = valid_num + 1
-                if valid_num != 0:
-                    kpt_score = kpt_score / valid_num
-                # rescoring
-                n_p['score'] = kpt_score * box_score
-
-            if self.soft_nms:
-                keep = soft_oks_nms(
-                    [img_kpts[i] for i in range(len(img_kpts))],
-                    oks_thre
-                )
-            else:
-                keep = oks_nms(
-                    [img_kpts[i] for i in range(len(img_kpts))],
-                    oks_thre
-                )
-
-            if len(keep) == 0:
-                oks_nmsed_kpts.append(img_kpts)
-            else:
-                oks_nmsed_kpts.append([img_kpts[_keep] for _keep in keep])
-
-        self._write_coco_keypoint_results(
-            oks_nmsed_kpts, res_file)
-        if 'test' not in self.image_set:
-            info_str = self._do_python_keypoint_eval(
-                res_file, res_folder)
-            name_value = OrderedDict(info_str)
-            return name_value, name_value['AP']
-        else:
-            return {'Null': 0}, 0
+    def evaluate(self, cfg, preds, output_dir, *args, **kwargs):
+        raise NotImplementedError
 
     def _write_coco_keypoint_results(self, keypoints, res_file):
         data_pack = [
